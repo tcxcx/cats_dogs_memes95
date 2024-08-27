0ee3dc1ba8dbfb3bfe4e157589ab35e4675a2c7b
FranItu <iturraldefj@gmail.com>
2024-08-28 01:42:44 +0200

    feat(Shufflepack): Quality Of life improvements at pack shuffle

--- packages/frontend/src/app/open-packs/page.tsx
+++ packages/frontend/src/app/open-packs/page.tsx
@@ -2,7 +2,7 @@ import CardsDeckViewer from "@/components/card-shuffle";
 
 export default function OpenPacks() {
   return (
-    <main className="flex min-h-screen max-w-7xl flex-col items-center justify-center">
+    <main className="flex min-h-screen max-w-full flex-col items-center justify-center">
       <CardsDeckViewer />{" "}
     </main>
   );
