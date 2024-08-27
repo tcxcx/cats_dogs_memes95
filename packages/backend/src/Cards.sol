68fe79ac123cd861767992ac95671ecb129605f1
Seven Cedars <Cedars7@proton.me>
2024-08-27 09:35:54 +0100

    added natspecs, inserted cardPackNo at openCardPack::Cards.sol

--- packages/backend/src/Cards.sol
+++ packages/backend/src/Cards.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
-// Basic ERC-721 or ERC-1155 contract. 
-// Manages cards, buying packs of cards. 
-// Buying packs of cards. use chainlink VRF for randomisation.    
-// Cards content + characteristics created by Itu. 
-
 pragma solidity ^0.8.0;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import {IAvatarExecutable} from "./AvatarBasedAccount.sol"; 
 
+/** 
+* ERC-1155 based contract that stores cards structs and manages their distribution. 
+* Integretation with chainlink VRF for randomisation is tbd.  
+*
+* authors: Argos, CriptoPoeta, 7cedars
+*/ 
 contract Cards is ERC1155 {
     /* errors */
     error Cards__ArraysNotSameLength(uint256 cardsLength, uint256 mintAmountLength);
@@ -36,9 +37,9 @@ contract Cards is ERC1155 {
         int16 volumeThreshold; 
         int16 areaThreshold; 
         int16 perimeterThreshold; 
-        uint16 ProbPull; // NB! in solidity nothing behing the comma. So we have to deal with this as full integeres
-        uint16 InfRange; // NB! in solidity nothing behing the comma. So we have to deal with this as full integeres
-        uint16 SupRange; // NB! in solidity nothing behing the comma. So we have to deal with this as full integeres
+        uint16 ProbPull; // NB! in solidity nothing behing the comma. So we have to deal with this as full integers
+        uint16 InfRange; // NB! in solidity nothing behing the comma. So we have to deal with this as full integers
+        uint16 SupRange; // NB! in solidity nothing behing the comma. So we have to deal with this as full integers
     }
     
 
@@ -134,10 +135,9 @@ contract Cards is ERC1155 {
     } 
 
     /**
-     * note function buy a pack of five random cards.
+     * note function to buy a pack of five random cards.
      * 
-     * note: the function does not have any params, but DOES take a value in native currency of the chain. 
-     * 
+     * @param cardPackNo The sequence number of the cardPack the player choose. It is used as salt in picking a random set of cards.   
      * 
      * note: the chance of a card appearing in a pack is defined by the amount of avaialble cards. In other words, rare cards are selected fewer times.
      * There is a (tiny) chance that a card that has been minted less than five times will be selected more than it exists. 
@@ -147,7 +147,7 @@ contract Cards is ERC1155 {
      * 
      * dev For now, payment occurs in native currency. Should this be in a ERC-20 coin instead? 
      */
-    function openCardPack() public payable onlyAvatarBasedAccount(msg.sender) {
+    function openCardPack(uint256 cardPackNo) public payable onlyAvatarBasedAccount(msg.sender) {
         uint256[] memory selectedCards = new uint256[](5); 
         uint256[] memory cardsValues = new uint256[](5); 
         uint256[] memory cardIds = s_cardIds; 
@@ -157,23 +157,23 @@ contract Cards is ERC1155 {
             revert Cards__InsufficientPayment(); 
         }
 
-        if (s_cardIds.length == 0) {
+        if (cardIds.length == 0) {
             revert Cards__NoCardsExist(); 
         }
 
         // step 1: get current balance owner of all existing cards. 
-        address[] memory addressArray = new address[](s_cardIds.length); 
+        address[] memory addressArray = new address[](cardIds.length); 
         for (uint256 i; i < s_cardIds.length; i++) {
             addressArray[i] = address(this); 
         }
-        uint256[] memory balances = balanceOfBatch(addressArray, s_cardIds); 
+        uint256[] memory balances = balanceOfBatch(addressArray, cardIds); 
         
         // step 2: create an _incremented_ array of all these balances 
         uint256[] memory incrementedArray = _incrementArray(balances);  // Note that the TOTAL amount of cards is the last value in this array. 
 
         // step 3: for each of the five cards, we retrieve a value between 0 and total amount of cards. This is used to select the cardId and add it to the CardPack object.
         for (uint256 i; i < 5; i++) {
-            uint256 pseudoRandomNumber = _pseudoRandomiser(i) % incrementedArray[incrementedArray.length - 1]; 
+            uint256 pseudoRandomNumber = _pseudoRandomiser(i + cardPackNo) % incrementedArray[incrementedArray.length - 1]; 
             uint256 cardId; 
             while (incrementedArray[cardId] < pseudoRandomNumber) {
                 cardId++;
@@ -193,6 +193,13 @@ contract Cards is ERC1155 {
         
     } 
 
+    /**
+     * note resetsPrice of a card pack. 
+     * 
+     * @param newPrice The new price of a card pack.     
+     *
+     * emits a ChangedCardPackPrice event. 
+     */
     function setPriceCardPack(uint256 newPrice) public onlyOwner {
         uint256 oldPrice = s_priceCardPack; 
 
@@ -201,6 +208,9 @@ contract Cards is ERC1155 {
         emit ChangedCardPackPrice(newPrice, oldPrice); 
     } 
 
+    /**
+     * note retrieves all accumulated funds from the contract.   
+     */
     function retrieveFunds() public onlyOwner {
         uint256 fullBalance = address(this).balance;  
         (bool success, ) = address(this).call{value: fullBalance}(''); 
@@ -214,7 +224,7 @@ contract Cards is ERC1155 {
     * Note: placeholder 'randomiser' function. Will be replaced with chainlink_VRF.
     * It will probably be best to call for a random number from chainlink once, and use it to return an array with five values. 
     */
-    function _pseudoRandomiser(uint256 salt) internal returns (uint256 pseudoRandomNumber) {
+    function _pseudoRandomiser(uint256 salt) internal view returns (uint256 pseudoRandomNumber) {
         pseudoRandomNumber = uint256(keccak256(abi.encodePacked(
             block.timestamp, 
             msg.sender, 
@@ -226,8 +236,8 @@ contract Cards is ERC1155 {
     } 
 
     // turns a normal array of values into an array that increments. 
-    function _incrementArray(uint256[] memory array) public view returns (uint256[] memory incrementArray) {
-        uint256[] memory incrementArray = new uint256[](array.length);  
+    function _incrementArray(uint256[] memory array) internal pure returns (uint256[] memory incrementArray) {
+        incrementArray = new uint256[](array.length);  
         
         for (uint256 i; i < array.length; i++) {
             if (i == 0) {
@@ -239,10 +249,21 @@ contract Cards is ERC1155 {
         return incrementArray;
     }
 
+    /** 
+    * Overrides the internal _setUri function from ERC-1155. It allows the function to be called in this contract. 
+    * 
+    * This is necessary to make updating the uri at a later date possible. 
+    */
     function _setURI(string memory newuri) internal override {
         super._setURI(newuri);
     }
-
+    
+    /** 
+    * Overrides the internal _safeBatchTransferFrom function from ERC-1155. It allows the function to be called in this contract. 
+    * 
+    * This is necessary to make transfer of cards without authorisation from the contract to the userAvatar possible. 
+    * The lack of authorisation is not a problem, because the function that calls this internalfunction ('openCardPack') has the necessary checks.  
+    */
     function _safeBatchTransferFrom(
         address from,
         address to,
@@ -257,7 +278,7 @@ contract Cards is ERC1155 {
 
     /* getter functions */
     /**
-     * notice: retrieves the collection of Cards of an Avatar Based Account. 
+     * Note: retrieves the collection of Cards of an Avatar Based Account. 
      *
      * dev: it returns an array with the length of all available cards. The values in this array represent the number of cards the account has of each card at that position of the array. 
      */
@@ -269,7 +290,7 @@ contract Cards is ERC1155 {
             addressArray[i] = AvatarBasedAccount; // this creates an array with the address of the Avatar based Account. 
         }
 
-        uint256[] memory cardCollection = balanceOfBatch(addressArray, s_cardIds);  
+        cardCollection = balanceOfBatch(addressArray, s_cardIds);  
         return cardCollection; 
     }
 
