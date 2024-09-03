dede3ee3a7a46cbf8437a3d2f4288df05f2f5de7
Tomas Cordero <tomas.cordero.esp@gmail.com>
2024-09-02 21:54:40 -0500

    feat(merge): integration preparation

--- packages/v1/apps/app/src/lib/types/index.d.ts
+++ packages/v1/apps/app/src/lib/types/index.d.ts
@@ -72,23 +72,23 @@ export type Score = [number, number]; // player1Points, player2Points
 
 // The GameState stores the current state of the game
 export type GameState = {
-    deckP1: Deck;
-    deckP2: Deck;
-    handP1: CardData[];
-    handP2: CardData[];
-    score: Score;
-    turnCount: number;
-    cardCollection: CardCollection;
-    powerList: PowerList;
-    typeList: TypeList;
+  deckP1: Deck;
+  deckP2: Deck;
+  handP1: CardData[];
+  handP2: CardData[];
+  score: Score;
+  turnCount: number;
+  cardCollection: CardCollection;
+  powerList: PowerList;
+  typeList: TypeList;
 };
 
 // ========================================
 
 // The result of a turn, indicating how many points each player scored
 export type TurnResult = {
-    player1Points: number;
-    player2Points: number;
+  player1Points: number;
+  player2Points: number;
 };
 
 // ========================================
@@ -113,22 +113,22 @@ export type GameLog = {
     currentScore: {
       player1Points: number;
       player2Points: number;
-    }
+    };
   }[];
-    winner: Winner;
+  winner: Winner;
 };
 
 // ========================================
 // camera view component
-export type Point = { x: number; y: number }
-export type Size = { width: number; height: number }
+export type Point = { x: number; y: number };
+export type Size = { width: number; height: number };
 
-export type Camera = { x: number; y: number; z: number }
+export type Camera = { x: number; y: number; z: number };
 export type Box = {
-	minX: number
-	minY: number
-	maxX: number
-	maxY: number
-	width: number
-	height: number
-}
+  minX: number;
+  minY: number;
+  maxX: number;
+  maxY: number;
+  width: number;
+  height: number;
+};
