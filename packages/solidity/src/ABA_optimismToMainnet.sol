d1839c842afbdb9ffea576145f11a3365c503712
Seven Cedars <Cedars7@proton.me>
2024-09-07 10:15:20 +0100

    clean tree

---
+++ packages/solidity/src/ABA_optimismToMainnet.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+/**
+ * Copied from TokenBounds's reference ERC6551Account.sol. (see lib/reference/src/examples/simple)
+ * Three changes:
+ * 1 - changed names of contracts. This ensure that the interfaceId of AvatarBasedAccounts is unique.
+ * 2 - added onERC1155Received and onERC1155BatchReceived. This allows receiving of ERC-1155 tokens.
+ * 3 - it integrates Chainlinks CCIP to allow it to be deployed on optimism sepolia and interact with contracts on mainnet sepolia. 
+ *
+ * It acts as a tokenised gateway between blockchains. Allowing users to seemlessly interact with the Cats, Dogs, Memes, etc game on mainnet.  
+ */
+
+// see https://docs.chain.link/ccip/tutorials/send-arbitrary-data for the docs. 
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+
+interface IAbaAccount {
+    receive() external payable;
+
+    function token() external view returns (uint256 chainId, address tokenContract, uint256 tokenId);
+
+    function state() external view returns (uint256);
+
+    function isValidSigner(address signer, bytes calldata context) external view returns (bytes4 magicValue);
+}
+
+interface IAbaExecutable {
+    function execute(address to, uint256 value, bytes calldata data, uint8 operation)
+        external
+        payable
+        returns (bytes memory);
+}
+
+contract ABA is IERC165, IERC1271, IAvatarAccount, IAvatarExecutable {
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
+    error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+    error FailedToWithdrawEth(address owner, address target, uint256 value); // Used when the withdrawal of Ether fails.
+    error DestinationChainNotAllowlisted(uint64 destinationChainSelector); // Used when the destination chain has not been allowlisted by the contract owner.
+    error SourceChainNotAllowlisted(uint64 sourceChainSelector); // Used when the source chain has not been allowlisted by the contract owner.
+    error SenderNotAllowlisted(address sender); // Used when the sender has not been allowlisted by the contract owner.
+    error InvalidReceiverAddress(); // Used when the receiver address is 0.
+
+    // Event emitted when a message is sent to another chain.
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        string text, // The text being sent.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the CCIP message.
+    );
+
+    uint256 public state;
+    // Mapping to keep track of allowlisted destination chains.
+    mapping(uint64 => bool) public allowlistedDestinationChains;
+
+    receive() external payable {}
+
+    constructor() {
+      CCIPReceiver(_router) 
+    }
+
+    function execute(address to, uint256 value, bytes calldata data, uint8 operation)
+        external
+        payable
+        virtual
+        returns (bytes memory result)
+    {
+        require(_isValidSigner(msg.sender), "Invalid signer");
+        require(operation == 0, "Only call operations are supported");
+
+        ++state;
+
+        bool success;
+        (success, result) = to.call{value: value}(data);
+
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    function isValidSigner(address signer, bytes calldata) external view virtual returns (bytes4) {
+        if (_isValidSigner(signer)) {
+            return IAvatarAccount.isValidSigner.selector;
+        }
+
+        return bytes4(0);
+    }
+
+    function isValidSignature(bytes32 hash, bytes memory signature) external view virtual returns (bytes4 magicValue) {
+        bool isValid = SignatureChecker.isValidSignatureNow(owner(), hash, signature);
+
+        if (isValid) {
+            return IERC1271.isValidSignature.selector;
+        }
+
+        return bytes4(0);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == type(IERC165).interfaceId || interfaceId == type(IAvatarAccount).interfaceId
+            || interfaceId == type(IAvatarExecutable).interfaceId;
+    }
+
+    function token() public view virtual returns (uint256, address, uint256) {
+        bytes memory footer = new bytes(0x60);
+
+        assembly {
+            extcodecopy(address(), add(footer, 0x20), 0x4d, 0x60)
+        }
+
+        return abi.decode(footer, (uint256, address, uint256));
+    }
+
+    function owner() public view virtual returns (address) {
+        (uint256 chainId, address tokenContract, uint256 tokenId) = token();
+        if (chainId != block.chainid) return address(0);
+
+        return IERC721(tokenContract).ownerOf(tokenId);
+    }
+
+    function _isValidSigner(address signer) internal view virtual returns (bool) {
+        return signer == owner();
+    }
+
+    /**
+     * @dev added onERC1155Received function to make single transfers of ERC1155 tokens to TBA possible.
+     */
+    function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    /**
+     * @dev added onERC1155Received function to make batch transfers of ERC1155 tokens to TBA possible.
+     */
+    function onERC1155BatchReceived(address, address, uint256[] memory, uint256[] memory, bytes memory)
+        public
+        virtual
+        returns (bytes4)
+    {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
